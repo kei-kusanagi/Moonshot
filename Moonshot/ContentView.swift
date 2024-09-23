b97fb2e62e7a36485285a9b81c874ebc36f85665
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-23 17:56:32 -0600

    Pushing new views onto the stack using NavigationLink

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,39 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        ScrollView {
-//            LazyVStack(spacing: 10) {
-//                ForEach(0..<100) {
-//                    CustomText("Item \($0)")
-//                        .font(.title)
-//                }
-//            }
-//            .frame(maxWidth: .infinity)
-//        }
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item \($0)")
-                        .font(.title)
+        NavigationStack {
+            NavigationLink("Tap Me") {
+                Text("Detail View")
+            }
+            .navigationTitle("SwiftUI")
+        }
+        NavigationStack {
+            NavigationLink {
+                Text("Detail View")
+            } label: {
+                VStack {
+                    Text("This is the label")
+                    Text("So is this")
+                    Image(systemName: "face.smiling")
+                }
+                .font(.largeTitle)
+            }
+        }
+        NavigationStack {
+            List(0..<100) { row in
+                NavigationLink("Row \(row)") {
+                    Text("Detail \(row)")
                 }
             }
+            .navigationTitle("SwiftUI")
         }
 
     }
 }
 
-struct CustomText: View {
-    let text: String
-
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creando un nuevo CustomText")
-        self.text = text
-    }
-}
 
 
 #Preview {
