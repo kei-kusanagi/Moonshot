2efbc3109181ae0d8da94087e321c66341aacf84
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-26 17:24:39 -0600

    challenge 2

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -48,38 +48,8 @@ struct MissionView: View {
                     .foregroundStyle(.lightBackground)
                     .padding(.vertical)
                 
-//                Text("Crew")
-//                    .font(.title.bold())
-//                    .padding(.bottom, 5)
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(crew, id: \.role) { crewMember in
-                            NavigationLink {
-                                AstronautView(astronaut: crewMember.astronaut)
-                            } label: {
-                                HStack {
-                                    Image(crewMember.astronaut.id)
-                                        .resizable()
-                                        .frame(width: 104, height: 72)
-                                        .clipShape(.capsule)
-                                        .overlay(
-                                            Capsule()
-                                                .strokeBorder(.white, lineWidth: 1)
-                                        )
-
-                                    VStack(alignment: .leading) {
-                                        Text(crewMember.astronaut.name)
-                                            .foregroundStyle(.white)
-                                            .font(.headline)
-                                        Text(crewMember.role)
-                                            .foregroundStyle(.white.opacity(0.5))
-                                    }
-                                }
-                                .padding(.horizontal)
-                            }
-                        }
-                    }
-                }
+                CrewScrollView(crew: crew)
+                
                 Rectangle()
                     .frame(height: 2)
                     .foregroundStyle(.lightBackground)
@@ -98,6 +68,7 @@ struct MissionView: View {
 
             }
             .padding(.bottom)
+            
         }
         .navigationTitle(mission.displayName)
         .navigationBarTitleDisplayMode(.inline)
@@ -111,6 +82,42 @@ struct MissionView: View {
                 return CrewMember(role: member.role, astronaut: astronaut)
             } else {
                 fatalError("Missing \(member.name)")
+            }
+        }
+    }
+}
+
+struct CrewScrollView: View {
+    let crew: [MissionView.CrewMember]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 104, height: 72)
+                                .clipShape(.capsule)
+                                .overlay(
+                                    Capsule()
+                                        .strokeBorder(.white, lineWidth: 1)
+                                )
+
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                Text(crewMember.role)
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
             }
         }
     }
